c0705503a8746b944551d4779c4a4100ddc9b7c8
joonblee <joon.bin.lee@cern.ch>
2019-12-26 18:11:00 +0900

    Update fig3.m

--- paper_work-BalancingFrequency/BalancingMethodForSplitNaturalFrequencies/fig3_Freq_PerfectWG/fig3.m
+++ paper_work-BalancingFrequency/BalancingMethodForSplitNaturalFrequencies/fig3_Freq_PerfectWG/fig3.m
@@ -17,24 +17,24 @@ freq_45=[]; freq_90=[]; freq_120=[];
 
 for phi0 = [pi/4 pi/2 2*pi/3]
 
-M = 5 % mass of the structure
-hOa=[0.01:0.01:0.1] % a/h
-a=((M*3/pi/rho)/(2+cos(phi0))/(1-cos(phi0))^2./(3*hOa+(hOa).^3/4)).^(1/3) % radius
-% 1/2 rho [ 4/3 pi (a+h/2)^3 - 4/3 pi (a-h/2)^3 ] = 5 kg
+  M = 5; % mass of the structure
+  hOa=[0.01:0.01:0.1]; % a/h
+  a=((M*3/pi/rho)/(2+cos(phi0))/(1-cos(phi0))^2./(3*hOa+(hOa).^3/4)).^(1/3); % radius
+  % 1/2 rho [ 4/3 pi (a+h/2)^3 - 4/3 pi (a-h/2)^3 ] = 5 kg
 
-h=a.*hOa % width
+  h=a.*hOa; % width
 
-mass=pi*rho/3*((a+h/2).^3-(a-h/2).^3)*(2+cos(phi0))*(1-cos(phi0))^2
-% https://en.wikipedia.org/wiki/Spherical_cap
-% It should be same with 'M'.
+  mass=pi*rho/3*((a+h/2).^3-(a-h/2).^3)*(2+cos(phi0))*(1-cos(phi0))^2;
+  % https://en.wikipedia.org/wiki/Spherical_cap
+  % It should be same with 'M'.
 
-%%% integrations %%%
-U_phi = int( tan(phi/2)^(2*n) / sin(phi)^3 ,phi,0,phi0);
-K_phi = int( tan(phi/2)^(2*n) * ((n+cos(phi))^2+2*sin(phi)^2) * sin(phi) ,phi,0,phi0);
+  %%% integrations %%%
+  U_phi = int( tan(phi/2)^(2*n) / sin(phi)^3 ,phi,0,phi0);
+  K_phi = int( tan(phi/2)^(2*n) * ((n+cos(phi))^2+2*sin(phi)^2) * sin(phi) ,phi,0,phi0);
 
-%%% natural frequency of the perfect HS (omega0) %%%
-omega0 = double(( n^2*(n^2-1)^2 * E*h.^2 ./ (3*(1+mu)*rho*a.^4) * U_phi/K_phi ) .^ .5) % angular freq
-NatFreq0 = double(omega0 / (2*pi)) % linear freq
+  %%% natural frequency of the perfect HS (omega0) %%%
+  omega0 = double(( n^2*(n^2-1)^2 * E*h.^2 ./ (3*(1+mu)*rho*a.^4) * U_phi/K_phi ) .^ .5); % angular freq
+  NatFreq0 = double(omega0 / (2*pi)); % linear freq
 
   if phi0 == pi/4
     freq_45 = NatFreq0;
@@ -46,8 +46,33 @@ NatFreq0 = double(omega0 / (2*pi)) % linear freq
 
 end
 
+%%%%%%%%%%%%%%%%%%%%%%%%
+% FEM (COMSOL) results %
+%%%%%%%%%%%%%%%%%%%%%%%%
+COMSOLx=[0.01;0.04;0.07;0.1];%hOa;
+COMSOLy_45=[57.534;354.77;727.55;1143.0];
+COMSOLy_90=[49.849;300.89;606.35;954.85];
+COMSOLy_120=[69.095;414.52;829.55;1270.4];
 
-%%% figure %%%
+ref_freq_45=[];
+ref_freq_90=[];
+ref_freq_120=[];
+
+for it_COMSOLx = 1:size(COMSOLx)
+  ref_COMSOLx = COMSOLx(it_COMSOLx);
+  for it_hOa = 1:10
+    if hOa(it_hOa) == ref_COMSOLx
+      ref_freq_45=[ref_freq_45; freq_45(it_hOa)];
+      ref_freq_90=[ref_freq_90; freq_90(it_hOa)];
+      ref_freq_120=[ref_freq_120; freq_120(it_hOa)];
+      break;
+    end
+  end
+end
+
+%%%%%%%%%%
+% figure %
+%%%%%%%%%%
 fig=figure(1);
 
 gcf;
@@ -55,54 +80,67 @@ fig.PaperPositionMode='auto';
 fig_pos=fig.PaperPosition;
 fig.PaperSize=[fig_pos(3) fig_pos(4)];
 
-grid on;
-% title('Eigenfrequencies of the perfect shell'); 
-xlabel('h/a'); 
-ylabel('Frequency [Hz]');  %('Natural Frequency (=\omega_0/2\pi) [Hz]');
+%%%%%%%%%%%%%
+% subplot 1 %
+%%%%%%%%%%%%%
+subplot1 = subplot(2,1,1);
 
-%%% plot (FEM) %%%
-COMSOLx=[0.01;0.04;0.07;0.1];%hOa;
-COMSOLy_45=[57.534;354.77;727.55;1143.0];
-COMSOLy_90=[49.849;300.89;606.35;954.85];
-COMSOLy_120=[69.095;414.52;829.55;1270.4];
-
-%%% plot (theory) %%%
-%figure(1);
-fig;
 hold on;
-plot1(1)=plot(hOa,freq_45,'-','color',[0 0 1],'LineWidth',1.1,'DisplayName','\phi_0=\pi/4 (Analytic)');
+p(1)=plot(hOa,freq_45,'-','color',[0 0 1],'LineWidth',1.1,'DisplayName','\phi_0=\pi/4 (Analytic)');
+p(2)=plot(COMSOLx,COMSOLy_45,'x','color',[0 0.4470 0.7410],'DisplayName','\phi_0=\pi/4 (FEM)');
+p(3)=plot(hOa,freq_90,'--','color',[1 0 0],'LineWidth',1.1,'DisplayName','\phi_0=\pi/2 (Analytic)');
+p(4)=plot(COMSOLx,COMSOLy_90,'o','color',[0.8500 0.3250 0.0980],'DisplayName','\phi_0=\pi/2 (FEM)');
+p(5)=plot(hOa,freq_120,':','color',[1 0 1],'LineWidth',1.1,'DisplayName','\phi_0=2\pi/3 (Analytic)');
+p(6)=plot(COMSOLx,COMSOLy_120,'^','color',[0.4940 0.1840 0.5560],'DisplayName','\phi_0=2\pi/3 (FEM)');
 
-%figure(1);
-fig;
+set(gca,'XTick',0.01:0.01:0.1);
+set(gca,'XTickLabel',{'','','','','','','','','',''});
+
+xlim([0.01 0.1]);
+
+ylabel('Frequency [Hz]','Interpreter','Latex');
+legend(p(1:6),'Location','northwest'); legend boxoff;
+
+%%%%%%%%%%%%%
+% subplot 2 %
+%%%%%%%%%%%%%
+subplot2 = subplot(2,1,2);
+
 hold on;
-plot1(2)=plot(COMSOLx,COMSOLy_45,'x','color',[0 0.4470 0.7410],'DisplayName','\phi_0=\pi/4 (FEM)');
+p(7)=plot(COMSOLx,(ref_freq_45-COMSOLy_45)./ref_freq_45,'-x','color',[0 0.4470 0.7410]);
+p(8)=plot(COMSOLx,(ref_freq_90-COMSOLy_90)./ref_freq_90,'--o','color',[0.8500 0.3250 0.0980]);
+p(9)=plot(COMSOLx,(ref_freq_120-COMSOLy_120)./ref_freq_120,':^','color',[0.4940 0.1840 0.5560]);
 
-%figure(1);
-fig;
-hold on;
-plot1(3)=plot(hOa,freq_90,'--','color',[1 0 0],'LineWidth',1.1,'DisplayName','\phi_0=\pi/2 (Analytic)');
+set(gca,'XTick',0.01:0.01:0.1);
+set(gca,'XTickLabel',{'0.01','0.02','0.03','0.04','0.05','0.06','0.07','0.08','0.09','0.1'});
 
-%figure(1);
-fig;
-hold on;
-plot1(4)=plot(COMSOLx,COMSOLy_90,'o','color',[0.8500 0.3250 0.0980],'DisplayName','\phi_0=\pi/2 (FEM)');
+set(gca,'YTick',0:0.05:0.2);
 
-%figure(1);
-fig;
-hold on;
-plot1(5)=plot(hOa,freq_120,':','color',[1 0 1],'LineWidth',1.1,'DisplayName','\phi_0=2\pi/3 (Analytic)');
+xlim([0.01 0.1]);
+ylim([0. 0.21]);
 
-%figure(1);
-fig;
-hold on;
-plot1(6)=plot(COMSOLx,COMSOLy_120,'^','color',[0.4940 0.1840 0.5560],'DisplayName','\phi_0=2\pi/3 (FEM)');
+xlabel('h/a','Interpreter','Latex'); 
+ylabel('$|\frac{\textrm{Analytic} - \textrm{FEM}}{\textrm{Analytic}}|$','Interpreter','Latex');
+% ylabel('$|\frac{Analytic - FEM}{Analytic}|$','Interpreter','Latex');
 
-xlim([0.01 0.1])
+%%%%%%%%%%%%%%%%%%%%%%
+% subplot properties %
+%%%%%%%%%%%%%%%%%%%%%%
+set(subplot1, 'position', [0.12,0.37,.85,.6]);
+set(subplot2,'position',[0.12,0.1,.85,.25]);
 
-%%% save figure %%%
-%figure(1); 
-fig; legend(plot1(1:6),'Location','northwest'); legend boxoff;
+subplot1;
+subplot2;
 
+grid(subplot1,'on');
+grid(subplot2,'on');
+
+subplot1.FontSize = 10;
+subplot2.FontSize = 10;
+
+%%%%%%%%%%%%%%%
+% save figure %
+%%%%%%%%%%%%%%%
 saveas(fig,'fig3.png');
 saveas(fig,'fig3.pdf');
 
